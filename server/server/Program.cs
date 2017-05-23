8ee6e6843a5d96c18c9c27cb6e47636a6387545a
rimlin <ilmirya@yandex.ru>
2017-05-23 23:32:04 +0300

    hui

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -24,18 +24,6 @@ namespace server
         
         static void Main(string[] args)
         {
-            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
-        @"Data Source=C:\Users\WorstOne\Source\Repos\pingpongNRserver2\server\server\db1.accdb;" +
-        @"User Id=;Password=;";
-            OleDbConnection connection = new OleDbConnection(connectionString);
-
-            string queryString = "INSERT INTO data (player1, player2, score1, score2, play_date) VALUES ('sda', 'asdasd', '1', '2', '10:00:00' )";
-
-            OleDbCommand command = new OleDbCommand(queryString, connection);
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close();
-
             RemotingConfiguration.Configure("server.exe.config", false);
             foreach (var i in ChannelServices.RegisteredChannels)
             {
